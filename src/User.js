66a4ef961abc8378d501a6ac4ef0da776ba2ca78
Abner <abnerluan.bonfim@gmail.com>
2024-12-12 14:58:25 -0300

    Dados dinamicos (Nome e imagem de perfil)

--- src/User.js
+++ src/User.js
@@ -1,11 +1,26 @@
+import React, {useState} from "react";
+
 export default function User() {
+  const [username, setUsername] = useState("catanacomics");
+  const [profileImg, setProfileImg] = useState("assets/img/catanacomics.svg");
+
+  const changeUsername = () => {
+    const newUsername = prompt('Digite o novo nome de usuário:');
+    if (newUsername) setUsername(newUsername);
+  };
+
+  const changeProfileImg = () => {
+    const newImg = prompt('Insira o link da nova imagem de perfil:');
+    if (newImg) setProfileImg(newImg);
+  };
+
   return (
     <div className="usuario">
-      <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+      <img src={profileImg} alt="imagem de perfil" onClick={changeProfileImg}/>
       <div className="texto">
         <span>
-          <strong>catanacomics</strong>
-          <ion-icon name="pencil"></ion-icon>
+          <strong>{username}</strong>
+          <ion-icon name="pencil" onClick={changeUsername}></ion-icon>
         </span>
       </div>
     </div>
